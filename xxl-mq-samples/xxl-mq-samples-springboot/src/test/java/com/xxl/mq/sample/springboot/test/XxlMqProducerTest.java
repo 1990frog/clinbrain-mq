c0f4c00c20a5cc0597797b3e120324846faaa598
liaopan <liaopan@clinbrain.com>
2021-06-23 15:09:13 +0800

    初始提交， 基于xxl-mq ，升级springboot2.5.1, xxl-rpc 升级到1.6

---
+++ xxl-mq-samples/xxl-mq-samples-springboot/src/test/java/com/xxl/mq/sample/springboot/test/XxlMqProducerTest.java
@@ -0,0 +1,28 @@
+package com.xxl.mq.sample.springboot.test;
+
+import com.xxl.mq.client.message.XxlMqMessage;
+import com.xxl.mq.client.producer.XxlMqProducer;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.TimeUnit;
+
+@SpringBootTest
+public class XxlMqProducerTest {
+
+    @Test
+    public void test() throws InterruptedException {
+
+        int msgNum = 1;
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < msgNum; i++) {
+            XxlMqProducer.produce(new XxlMqMessage("topic_1", "Data:"+i));
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("msgNum = " + msgNum + ", cost = " + (end-start));
+
+        // async send msg, wait
+        TimeUnit.SECONDS.sleep(30);
+    }
+
+}
