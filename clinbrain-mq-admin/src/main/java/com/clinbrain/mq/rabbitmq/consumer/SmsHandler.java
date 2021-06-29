bd054afde1560180f4f221fc23272fc3af220797
zhuangxupeng <>
2021-06-29 16:51:08 +0800

    消费端开启消息确认

--- clinbrain-mq-admin/src/main/java/com/clinbrain/mq/rabbitmq/consumer/SmsHandler.java
+++ clinbrain-mq-admin/src/main/java/com/clinbrain/mq/rabbitmq/consumer/SmsHandler.java
@@ -7,16 +7,21 @@ import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
-import java.io.UnsupportedEncodingException;
-
 @Slf4j
 @Component
 public class SmsHandler {
 
     @RabbitListener(queues = {QueueConfig.QUEUE_SMS_INFORM})
-    public void receiveSms(Message message, Channel channel) throws UnsupportedEncodingException {
-        byte[] body = message.getBody();
-        String msg = new String(body,"utf-8");
-        log.info("queue sms inform receive msg={}",msg);
+    public void receiveSms(Message message, Channel channel) {
+        try {
+            byte[] body = message.getBody();
+            String msg = new String(body,"utf-8");
+
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
+
+            log.info("queue sms inform receive msg={}",msg);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
     }
 }
